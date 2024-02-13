da5a00d2c510e0ad9e1ef1c896dfa062b3ba8a9d
Emre Ermisoglu <emreermisoglu@gmail.com>
2024-02-12 22:43:18 -0500

    Address 5

--- src/main/java/com/bittercode/model/Address.java
+++ src/main/java/com/bittercode/model/Address.java
@@ -8,6 +8,7 @@ public class Address implements Serializable {
     private String addressLine2;
     private String addressLine3;
     private String addressLine4;
+    private String addressLine5;
     private String city;
     private String state;
     private String country;
